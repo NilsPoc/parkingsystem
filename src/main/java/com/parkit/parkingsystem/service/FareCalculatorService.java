28c4343cf5032a59e7cf16c03ff20d2dc148a732
Nils <nils.pochat@gmail.com>
2022-02-02 16:27:37 +0100

    fix negative durations, make the unit tests pass

--- src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
+++ src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
@@ -1,5 +1,9 @@
 package com.parkit.parkingsystem.service;
 
+import java.util.concurrent.TimeUnit;
+
+import org.apache.commons.math3.util.Precision;
+
 import com.parkit.parkingsystem.constants.Fare;
 import com.parkit.parkingsystem.model.Ticket;
 
@@ -10,19 +14,32 @@ public class FareCalculatorService {
             throw new IllegalArgumentException("Out time provided is incorrect:"+ticket.getOutTime().toString());
         }
 
+        /**
         int inHour = ticket.getInTime().getHours();
         int outHour = ticket.getOutTime().getHours();
-
         //TODO: Some tests are failing here. Need to check if this logic is correct
         int duration = outHour - inHour;
-
+        */
+        
+        // int > double et transformer getHours and getTime > faire la soustraction en ms > convertir les ms en heures  
+        
+        Long inHourInMillis = ticket.getInTime().getTime();
+        Long outHourInMillis = ticket.getOutTime().getTime();
+       
+        
+        double durationMin = (double) TimeUnit.MILLISECONDS.toMinutes(outHourInMillis - inHourInMillis);
+        double durationHour = Precision.round((durationMin/60), 2);
+        
+        
         switch (ticket.getParkingSpot().getParkingType()){
             case CAR: {
-                ticket.setPrice(duration * Fare.CAR_RATE_PER_HOUR);
+               // ticket.setPrice(duration * Fare.CAR_RATE_PER_HOUR);
+            	ticket.setPrice(durationHour * Fare.CAR_RATE_PER_HOUR);
                 break;
             }
             case BIKE: {
-                ticket.setPrice(duration * Fare.BIKE_RATE_PER_HOUR);
+               // ticket.setPrice(duration * Fare.BIKE_RATE_PER_HOUR);
+            	ticket.setPrice(durationHour * Fare.BIKE_RATE_PER_HOUR);
                 break;
             }
             default: throw new IllegalArgumentException("Unkown Parking Type");
